21499f5027fbe1d208a5c833b1c3b4fe10a5b7ea
Samir Wadhwania <swadhwania96@gmail.com>
2018-11-15 12:56:18 -0500

    Hard distill data

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill" --hard_distill_ep 1500 --seed 6
-python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill" --hard_distill_ep 1500 --seed 7
-python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill" --hard_distill_ep 1500 --seed 8
-python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill" --hard_distill_ep 1500 --seed 9
-python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill" --hard_distill_ep 1500 --seed 10
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 1
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 2
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 3
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 4
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 5
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 6
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 7
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 8
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 9
+python main.py simple_spread_flip init_graph --n_episodes 6000 --flip_ep 1500 --log_comment "distill_256" --hard_distill_ep 1500 --seed 10
+git add .
+git commit -m "Hard distill data"
+git push origin analyzing_flip
