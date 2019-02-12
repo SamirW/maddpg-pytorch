283ab3ad17416559985d629abde05b79422a514c
Samir Wadhwania <swadhwania96@gmail.com>
2019-02-12 11:45:20 -0500

    Get data

--- get_data.sh
+++ get_data.sh
@@ -1,27 +1,12 @@
 #!/usr/bin/env bash
-# python main.py simple_spread_flip analyze_vf_flipped_input --seed 5 --n_episodes 1500 --hard_distill_ep 1500 --log_comment "heatmap_data" --save_buffer --display_every 100
+python main.py simple_spread_flip eval_graph_new --seed 5 --n_episodes 1500 --hard_distill_ep 1500 --log_comment "heatmap_data" --save_buffer
 
-# python main.py simple_spread_flip analyze_vf --seed 3 --n_episodes 2500 --model_save_freq 250 --flip_ep 750 --hard_distill_ep 750 --eval_ep 750 --log_comment "distill_eval"
-# python main.py simple_spread_flip analyze_vf_flipped --seed 3 --n_episodes 2500 --model_save_freq 250 --flip_ep 0 --log_comment "baseline" 
-# python main.py simple_spread_flip analyze_vf_flipped_input --seed 3 --n_episodes 4000 --model_save_freq 250 --flip_ep 1500 --log_comment "no_distill_learn" 
-# python main.py simple_spread_flip analyze_vf_flipped_input --seed 3 --n_episodes 4000 --model_save_freq 250 --flip_ep 1500 --hard_distill_ep 1500 --log_comment "distill_learn"
-# python main.py simple_spread_flip analyze_vf_flipped_input --seed 3 --n_episodes 4000 --model_save_freq 250 --flip_ep 1500 --hard_distill_ep 1500 --skip_actor_length 500 --log_comment "distill_learn_skip_500"
-python main.py simple_spread_flip analyze_vf_flipped_input --seed 3 --n_episodes 4000 --model_save_freq 250 --flip_ep 1500 --hard_distill_ep 1500 --hard_distill_only_actor --log_comment "distill_only_actor"
-
-# python main.py simple_spread_flip analyze_vf --seed 4 --n_episodes 2500 --flip_ep 0 --log_comment "baseline" 
-# python main.py simple_spread_flip analyze_vf --seed 4 --n_episodes 2500 --flip_ep 750 --eval_ep 750 --log_comment "no_distill_eval" 
-# python main.py simple_spread_flip analyze_vf --seed 4 --n_episodes 2500 --flip_ep 750 --hard_distill_ep 750 --eval_ep 750 --log_comment "distill_eval"
-# python main.py simple_spread_flip analyze_vf --seed 4 --n_episodes 2500 --flip_ep 750 --hard_distill_ep 750 --log_comment "distill_learn"
-# python main.py simple_spread_flip analyze_vf --seed 4 --n_episodes 2500 --flip_ep 750 --hard_distill_ep 750 --skip_actor_length 250 --log_comment "distill_learn_skip_1000"
-
-# python main.py simple_spread_flip analyze_vf --seed 5 --n_episodes 2500 --flip_ep 0 --log_comment "baseline" 
-# python main.py simple_spread_flip analyze_vf --seed 5 --n_episodes 2500 --flip_ep 750 --eval_ep 750 --log_comment "no_distill_eval" 
-# python main.py simple_spread_flip analyze_vf --seed 5 --n_episodes 2500 --flip_ep 750 --hard_distill_ep 750 --eval_ep 750 --log_comment "distill_eval"
-# python main.py simple_spread_flip analyze_vf --seed 5 --n_episodes 2500 --flip_ep 750 --hard_distill_ep 750 --log_comment "distill_learn"
-# python main.py simple_spread_flip analyze_vf --seed 5 --n_episodes 2500 --flip_ep 750 --hard_distill_ep 750 --skip_actor_length 250 --log_comment "distill_learn_skip_1000"
-
-# python main.py simple_spread_flip_6 eval_graph --seed 3 --n_episodes 4000 --log_comment "heatmap_data" --save_buffer --display_every 100
-# python main.py simple_spread_flip_6 eval_graph --seed 3 --n_episodes 10000 --flip_ep 0 --log_comment "baseline"
-# python main.py simple_spread_flip_6 eval_graph --seed 3 --n_episodes 10000 --flip_ep 4000 --hard_distill_ep 4000 --eval_ep 4000 --log_comment "distill_eval" --display_every 250
-# python main.py simple_spread_flip_6 eval_graph --seed 3 --n_episodes 10000 --flip_ep 4000 --hard_distill_ep 4000 --log_comment "distill_learn"
-# python main.py simple_spread_flip_6 eval_graph --seed 3 --n_episodes 10000 --flip_ep 4000 --hard_distill_ep 4000 --skip_actor_length 1000 --log_comment "distill_learn_skip_1000"
+seed = 3
+while [ $seed -le 13 ]
+do
+	python main.py simple_spread_flip analyze_vf_flipped_input --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 1500 --log_comment "no_distill" 
+	python main.py simple_spread_flip analyze_vf_flipped_input --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 1500 --hard_distill_ep 1500 --log_comment "distill"
+	python main.py simple_spread_flip analyze_vf_flipped_input --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 1500 --hard_distill_ep 1500 --distill_pass_actor --log_comment "distill_pass_actor"
+	python main.py simple_spread_flip analyze_vf_flipped_input --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 1500 --hard_distill_ep 1500 --distill_pass_critic --log_comment "distill_pass_critic"
+	((seed++))
+done
