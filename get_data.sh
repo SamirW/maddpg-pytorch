9f3f1321f967dcfc244a670e5095c01fb4ab54f3
Samir Wadhwania <swadhwania96@gmail.com>
2019-02-14 17:59:16 -0500

    some data

--- get_data.sh
+++ get_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python main.py simple_spread_flip_3 test --seed 3 --n_episodes 2500 --log_comment "heatmap_data" --save_buffer
+# python main.py simple_spread_flip_3 test --seed 3 --n_episodes 3000 --log_comment "heatmap_data" --save_buffer
 
 seed=3
-while [ $seed -lt 4 ];
+while [ $seed -lt 9 ];
 do
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --log_comment "no_distill" --display_every 100 
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --hard_distill_ep 2500 --log_comment "distill" --display_every 100
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --hard_distill_ep 2500 --distill_pass_actor --log_comment "distill_pass_actor"
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --hard_distill_ep 2500 --distill_pass_critic --log_comment "distill_pass_critic"
+	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 5000 --model_save_freq 500 --flip_ep 3000 --log_comment "no_distill"
+	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 5000 --model_save_freq 500 --flip_ep 3000 --hard_distill_ep 3000 --log_comment "distill"
+	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 5000 --model_save_freq 500 --flip_ep 3000 --hard_distill_ep 3000 --distill_pass_actor --log_comment "distill_pass_actor"
+	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 5000 --model_save_freq 500 --flip_ep 3000 --hard_distill_ep 3000 --distill_pass_critic --log_comment "distill_pass_critic"
 	((seed++))
 done
