926463fe0c26a6e689f669591d236cf9d89aaf92
Samir Wadhwania <swadhwania96@gmail.com>
2019-02-14 22:09:42 -0500

    clean

--- get_data.sh
+++ get_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-python main.py simple_spread_flip_3 test --seed 3 --n_episodes 2500 --log_comment "heatmap_data" --save_buffer
+seed=1
 
-seed=3
-while [ $seed -lt 4 ];
+python main.py simple_spread_flip_3 eval_graph --seed $seed --n_episodes 3000 --log_comment "heatmap_data" --save_buffer
+
+while [ $seed -le 10 ];
 do
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --log_comment "no_distill" --display_every 100 
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --hard_distill_ep 2500 --log_comment "distill" --display_every 100
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --hard_distill_ep 2500 --distill_pass_actor --log_comment "distill_pass_actor"
-	python main.py simple_spread_flip_3 test --seed $seed --n_episodes 4000 --model_save_freq 100 --flip_ep 2500 --hard_distill_ep 2500 --distill_pass_critic --log_comment "distill_pass_critic"
+	python main.py simple_spread_flip_3 eval_graph --seed $seed --n_episodes 12000 --model_save_freq 1000 --flip_ep 3000 --log_comment "no_distill" 
+	python main.py simple_spread_flip_3 eval_graph --seed $seed --n_episodes 12000 --model_save_freq 1000 --flip_ep 3000 --hard_distill_ep 3000 --log_comment "distill"
+	python main.py simple_spread_flip_3 eval_graph --seed $seed --n_episodes 12000 --model_save_freq 1000 --flip_ep 3000 --hard_distill_ep 3000 --distill_pass_actor --log_comment "distill_pass_actor"
+	python main.py simple_spread_flip_3 eval_graph --seed $seed --n_episodes 12000 --model_save_freq 1000 --flip_ep 3000 --hard_distill_ep 3000 --distill_pass_critic --log_comment "distill_pass_critic"
 	((seed++))
 done
--- git_pull.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-while [ 1 ]
-do 
-	git pull origin sharing
-	echo "Waiting 2 minutes..."
-	sleep 2m
-done
