997f5f25cec80b63d374d36ca09d9862102c5c59
Samir Wadhwania <swadhwania96@gmail.com>
2019-02-28 08:20:33 -0500

    data file

--- get_data.sh
+++ get_data.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
 seed=1
 
-python main.py simple_spread_flip test --seed $seed --n_episodes 1000 --log_comment "test" --flip_ep 500 --display_every 100 --flip_critic
+python main.py simple_spread_hard eval_graph_relative \
+--seed $seed \
+--init_noise_scale 0.7 \
+--episode_length 50  \
+--n_episodes 15000 \
+--flip_ep 6000 \
+--log_comment "no_distill" \
 
-# while [ $seed -le 3 ];
-# do
-# 	python main.py simple_spread_hard eval_graph_relative --seed $seed --init_noise_scale 0.7 --episode_length 50 --n_episodes 15000 --flip_ep 7500  --log_comment "no_distill"
-# 	python main.py simple_spread_hard eval_graph_relative --seed $seed --init_noise_scale 0.7 --episode_length 50 --n_episodes 15000 --flip_ep 7500  --eval_ep 7500 --log_comment "no_distill_eval"
-# 	python main.py simple_spread_hard eval_graph_relative --seed $seed --init_noise_scale 0.7 --episode_length 50 --n_episodes 15000 --flip_ep 7500 --hard_distill_ep 7500 --log_comment "distill"
-# 	python main.py simple_spread_hard eval_graph_relative --seed $seed --init_noise_scale 0.7 --episode_length 50 --n_episodes 15000 --flip_ep 7500 --hard_distill_ep 7500 --eval_ep 7500 --log_comment "distill_eval"
-# 	# python main.py simple_spread_flip_4 eval_graph_relative --seed $seed --n_episodes 15000 --flip_ep 4000 --hard_distill_ep 4000 --distill_pass_actor --log_comment "distill_pass_actor"
-# 	# python main.py simple_spread_flip_4 eval_graph_relative --seed $seed --n_episodes 15000 --flip_ep 4000 --hard_distill_ep 4000 --distill_pass_critic --log_comment "distill_pass_critic"
-# 	((seed++))
-# done
+python main.py simple_spread_hard eval_graph_relative \
+--seed $seed \
+--init_noise_scale 0.7 \
+--episode_length 50  \
+--n_episodes 15000 \
+--flip_ep 6000 \
+--distill_ep 6000 \
+--log_comment "distill_all" \
+
+python main.py simple_spread_hard eval_graph_relative \
+--seed $seed \
+--init_noise_scale 0.7 \
+--episode_length 50  \
+--n_episodes 15000 \
+--flip_ep 6000 \
+--distill_ep 6000 \
+--log_comment "critic_only" \
+
+python main.py simple_spread_hard eval_graph_relative \
+--seed $seed \
+--init_noise_scale 0.7 \
+--episode_length 50  \
+--n_episodes 15000 \
+--flip_ep 6000 \
+--distill_ep 6000 \
+--distill_pass_critic \
+--log_comment "actor_only" \
+
+python main.py simple_spread_hard eval_graph_relative \
+--seed $seed \
+--init_noise_scale 0.7 \
+--episode_length 50  \
+--n_episodes 15000 \
+--flip_ep 6000 \
+--distill_ep 6000 \
+--distill_pass_critic \
+--flip_critic \
+--log_comment "ours" \
+
+python main.py simple_spread_hard eval_graph_relative \
+--seed $seed \
+--init_noise_scale 0.7 \
+--episode_length 50  \
+--n_episodes 15000 \
+--flip_ep 6000 \
+--log_comment "critic_flip_only" \
+--flip_critic \
