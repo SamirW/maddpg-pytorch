2d9f658dd5e2ea67cd90ba7572f44a2a41d033f3
Dong Ki Kim <dkkim93@mit.edu>
2019-02-28 08:42:15 -0500

    Script changes for aws

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
+function print_header(){
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    echo $1
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+# Directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+
+# Tensorboard
+pkill tensorboard
+
+# Virtualenv
+cd $DIR
+virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+# Add baseline package to path
+export PYTHONPATH=$DIR/thirdparty/multiagent-particle-envs:$PYTHONPATH
+
+# Train tf 
+print_header "Training network"
+cd $DIR
+
+# Comment for using GPU
+export CUDA_VISIBLE_DEVICES=-1
+
 seed=1
 
-python main.py simple_spread_hard eval_graph_relative \
+python3.6 main.py simple_spread_hard eval_graph_relative \
 --seed $seed \
 --init_noise_scale 0.7 \
 --episode_length 50  \
@@ -9,7 +37,7 @@ python main.py simple_spread_hard eval_graph_relative \
 --flip_ep 6000 \
 --log_comment "no_distill" \
 
-python main.py simple_spread_hard eval_graph_relative \
+python3.6 main.py simple_spread_hard eval_graph_relative \
 --seed $seed \
 --init_noise_scale 0.7 \
 --episode_length 50  \
@@ -18,7 +46,7 @@ python main.py simple_spread_hard eval_graph_relative \
 --distill_ep 6000 \
 --log_comment "distill_all" \
 
-python main.py simple_spread_hard eval_graph_relative \
+python3.6 main.py simple_spread_hard eval_graph_relative \
 --seed $seed \
 --init_noise_scale 0.7 \
 --episode_length 50  \
@@ -27,7 +55,7 @@ python main.py simple_spread_hard eval_graph_relative \
 --distill_ep 6000 \
 --log_comment "critic_only" \
 
-python main.py simple_spread_hard eval_graph_relative \
+python3.6 main.py simple_spread_hard eval_graph_relative \
 --seed $seed \
 --init_noise_scale 0.7 \
 --episode_length 50  \
@@ -37,7 +65,7 @@ python main.py simple_spread_hard eval_graph_relative \
 --distill_pass_critic \
 --log_comment "actor_only" \
 
-python main.py simple_spread_hard eval_graph_relative \
+python3.6 main.py simple_spread_hard eval_graph_relative \
 --seed $seed \
 --init_noise_scale 0.7 \
 --episode_length 50  \
@@ -48,7 +76,7 @@ python main.py simple_spread_hard eval_graph_relative \
 --flip_critic \
 --log_comment "ours" \
 
-python main.py simple_spread_hard eval_graph_relative \
+python3.6 main.py simple_spread_hard eval_graph_relative \
 --seed $seed \
 --init_noise_scale 0.7 \
 --episode_length 50  \
--- get_data_4_agent.sh
+++ get_data_4_agent.sh
@@ -1,27 +1,55 @@
 #!/bin/bash
+function print_header(){
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    echo $1
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+# Directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+
+# Tensorboard
+pkill tensorboard
+
+# Virtualenv
+cd $DIR
+virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+# Add baseline package to path
+export PYTHONPATH=$DIR/thirdparty/multiagent-particle-envs:$PYTHONPATH
+
+# Train tf 
+print_header "Training network"
+cd $DIR
+
+# Comment for using GPU
+export CUDA_VISIBLE_DEVICES=-1
+
 seed=1
 
-python main.py simple_spread_flip_4 eval_graph_relative \
+python3.6 main.py simple_spread_flip_4 eval_graph_relative \
 --seed $seed \
 --n_episodes 15000 \
 --flip_ep 5000 \
 --log_comment "no_distill" \
 
-python main.py simple_spread_flip_4 eval_graph_relative \
+python3.6 main.py simple_spread_flip_4 eval_graph_relative \
 --seed $seed \
 --n_episodes 15000 \
 --flip_ep 5000 \
 --distill_ep 5000 \
 --log_comment "distill_all" \
 
-python main.py simple_spread_flip_4 eval_graph_relative \
+python3.6 main.py simple_spread_flip_4 eval_graph_relative \
 --seed $seed \
 --n_episodes 15000 \
 --flip_ep 5000 \
 --distill_ep 5000 \
 --log_comment "critic_only" \
 
-python main.py simple_spread_flip_4 eval_graph_relative \
+python3.6 main.py simple_spread_flip_4 eval_graph_relative \
 --seed $seed \
 --n_episodes 15000 \
 --flip_ep 5000 \
@@ -29,7 +57,7 @@ python main.py simple_spread_flip_4 eval_graph_relative \
 --distill_pass_critic \
 --log_comment "actor_only" \
 
-python main.py simple_spread_flip_4 eval_graph_relative \
+python3.6 main.py simple_spread_flip_4 eval_graph_relative \
 --seed $seed \
 --n_episodes 15000 \
 --flip_ep 5000 \
@@ -38,7 +66,7 @@ python main.py simple_spread_flip_4 eval_graph_relative \
 --flip_critic \
 --log_comment "ours" \
 
-python main.py simple_spread_flip_4 eval_graph_relative \
+python3.6 main.py simple_spread_flip_4 eval_graph_relative \
 --seed $seed \
 --n_episodes 15000 \
 --flip_ep 5000 \
